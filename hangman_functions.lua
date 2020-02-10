bd5bf00c15d70343f7fa562e071c9d654de6f9a0
Michael Dahlquist <mwdahl22@g.holycross.edu>
2020-02-09 23:31:56 -0500

    added prolog descr, added func comments, cleaned comments,

--- hangman_functions.lua
+++ hangman_functions.lua
@@ -3,11 +3,16 @@ Names: Michael Dahlquist & Kristen Qako
 Class: csci324 - spring2020
 File: "hangman_functions.lua"
 Program Description: These are the functions for the game of hangman.
+                     This file is ran at the beginning of "hangman.lua" to
+                     define all of the functions. Each function is further
+                     described within itself.
 --]]
 
---A portion of this code was inspired by https://stackoverflow.com/questions/11201262/how-to-read-data-from-a-file-in-lua
---This section of the code reads through dictionary.txt and inserts all of the words into a table
-function getwords(file_name)
+function get_lines(file_name)
+--A portion of this code was inspired by:
+--https://stackoverflow.com/questions/11201262/how-to-read-data-from-a-file-in-lua
+--get_lines(file_name) returns a table and count of all the lines in file_name
+--does not include return characters
     new_table = {}
     count = 0
     local file = io.open(file_name, "rb")
@@ -17,29 +22,29 @@ function getwords(file_name)
     end
     file:close()
     return new_table, count
-
 end
 
 
-function tableLength(thisTable)
-  --this function was found at the following url
+function table_size(this_table)
+  --this function was found at the following url:
   --https://www.quora.com/How-do-I-get-the-number-of-elements-in-an-array-with-Lua
+  --table_size returns the count of all of the items in thisTable
   counter = 0
-  for each in pairs(thisTable) do
+  for each in pairs(this_table) do
     counter = counter+1
   end
   return counter
 end
 
---These are the graphics for the game. It prints the remaining number of moves as well as the gallow. It is updated as the user makes guesses in hangman.lua
 function gallow(num)
+--These are the graphics for the game. It prints the remaining number of moves
+--as well as the gallow. It is updated as the user makes guesses in hangman.lua
   a = ""
   b = ""
   c = " "
   d = ""
   e = ""
   f = ""
-
   if num < 5 then print("You have "..6-num.." attempts left") end
   if num >= 1 then a = "O" end
   if num >= 2 then b = "|" end
@@ -62,6 +67,7 @@ function gallow(num)
 end
 
 function gallow_test()
+--gallow_test(num) prints all prossible attempts leves (0-6)
   for i = 0, 6 do
     print("gallow("..i.."):")
     gallow(i)
