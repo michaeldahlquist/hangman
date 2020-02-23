e47fa6ce27ab4e1576216ea525960e4d3ce27628
Michael Dahlquist <mwdahl22@g.holycross.edu>
2020-02-23 16:46:31 -0500

    lots of changes :)

--- hangman.lua
+++ hangman.lua
@@ -20,23 +20,23 @@ print("Hello user, welcome to hangman")
 
 --This section creates a table with every word in dictionary.txt as elements.
 file_name = "dictionary.txt"
-words, words_length = get_lines(file_name)
+words = get_lines(file_name) --returns words{table}
 
 
---This shows the user the size of the table
-print(words_length.." words in table")
+play_game = true
+won_count = 0
+while play_game do
+    play_game, game_won = hangman(words)
+    if game_won then
+        won_count = won_count + 1
+    end
+end
 
---This section finds a random index of the table, meaning the word is randomly picked.
-math.randomseed(os.time()) --need to run first
-random_index = math.random(1,words_length)
-
---prints random index and the word
-print("Randomly picked "..random_index)
---print(words[random_index])
-
---calculates and prints the word_length
-word_length = string.len( words[random_index] )
-print("Word size is "..word_length)
+if won_count < 1 then
+    print ("You tried... 😂")
+else
+    print("You won "..won_count.." games!")
+end
 
 --[[
 Since we have word length, can't we just make a for loop? Would that be it,
@@ -48,109 +48,9 @@ Also this is how you block comment
 
 --]]
 
---This initializes each letter of the word to false as a way to test if the
---user inputs a correct letter later in the code.
-show_letter = {} --better name for this variable??
-for i = 1, word_length do --this initalized them to false
-    show_letter[i] = false
-end
 
---Empty table that will store the users guesses.
-letters_guessed = {}
 
-wrong_ct = 0 --number of times user guessed wrong
-correct_ct = 0--number of times user guessed correctly
-word = words[random_index]
 
---This function prints out the players incorrect letter guesses.
---It is in order of appearance.
-function print_wrong(letters_guessed, wrong_ct)
-    print("Guessed incorrect letters:")
-    for i = 0, wrong_ct-1 do
-        io.write(letters_guessed[i].." ")
-    end
-end
-
---I made the line printing a function that calls gallow with the attempt
---We could probably make a game function and just have the game in a loop
-
---This function is what runs the game.
-while (wrong_ct < 6) and (correct_ct < string.len(word)) do
-    --this while loop runs until the man in hanged or the user gets the word.
-    gallow(wrong_ct)
-    io.write("Your word is:\n")
-    for i = 1, string.len(word) do --prints underscore for hidden letters and
-        if show_letter[i] then     --prints the letters if guessed correctly
-            io.write(string.sub(word,i,i).." ")--we would print the ith character
-        else
-            io.write("_ ")
-        end
-    end
-    print()
-    print_wrong(letters_guessed, wrong_ct)
-    print()
-
-  --This section of code takes users guesses and checks whether a wrong input
-  --has been guessed before. We decided that letters that have been shown
-  --should not display that output and we just continue to show the current game.
-    io.write("Please guess a letter: ")
-    ch = io.read(2); --reads the input
-    ch = string.upper(string.sub(ch,1,1) )
-    char_found = false;
-    char_unique = false;
-    while not char_unique do
-        char_unique = true
-        for k = 0, wrong_ct do
-            if letters_guessed[k] == ch then --checks if letter has been guessed
-                io.write("You already guessed that, input a new letter:")
-                ch = io.read(2);
-                ch = string.upper( string.sub(ch,1,1) )
-                char_unique = false
-            end
-        end
-    end
- --This section of code checks whether the user input is correct. If it is
- --correct, we make that index of the word true, then increment the number of
- --correctly counted words.
-    for i = 1, string.len(word) do
-        --compares letter in word to the users guess
-        if string.upper(string.sub(word,i,i)) == ch then
-            if show_letter[i] then
-                char_found = true
-            else
-                char_found = true
-                show_letter[i] = true;
-                correct_ct = correct_ct + 1
-            end
-        end
-    end
-
-    if not char_found then
-        letters_guessed[wrong_ct] = ch
-        wrong_ct = wrong_ct + 1
-    end
-    print()
-end
-
-gallow(wrong_ct)
-io.write("Your word is:\n")
-for i = 1, string.len(word) do
-    if show_letter[i] or wrong_ct == 6 then
-        io.write(string.sub(word,i,i).." ")--here we would print the ith character
-    else
-        io.write("_ ")
-    end
-end
-print()
-print_wrong(letters_guessed, wrong_ct)
-print()
-print()
-if wrong_ct < 6 then print("Congratulations, you guessed the word!")
-else print("Sorry, you lost! The word was: ", word)
-end
-print()
-
---END HANGMAN
 
 --TODO:
 --Add the hangman game into a function
--- hangman_functions.lua
+++ hangman_functions.lua
@@ -21,7 +21,7 @@ function get_lines(file_name)
         new_table[count] = string.sub(line, 1, string.len(line)-1)
     end
     file:close()
-    return new_table, count
+    return new_table
 end
 
 
@@ -39,31 +39,34 @@ end
 function gallow(num)
 --These are the graphics for the game. It prints the remaining number of moves
 --as well as the gallow. It is updated as the user makes guesses in hangman.lua
-  a = ""
-  b = ""
-  c = " "
-  d = ""
-  e = ""
-  f = ""
-  if num < 5 then print("You have "..6-num.." attempts left") end
-  if num >= 1 then a = "O" end
-  if num >= 2 then b = "|" end
-  if num >= 3 then c = "/" end
-  if num >= 4 then d = "\\" end
-  if num >= 5 then e = "/" end
-  if num == 5 then print("You have 1 attempt left") end
-  if num == 6 then f = "\\" end
-  print("    ___________")
-  print("    |         |")
-  print("    |         "..a)
-  print("    |        "..c..b..d)
-  print("    |        "..e.." "..f)
-  print("    |")
-  print("    |")
-  print("    |")
-  print("    |")
-  print("    |")
-  print("____|____")
+    for i = 1, 10 do
+        print()
+    end
+    a = ""
+    b = ""
+    c = " "
+    d = ""
+    e = ""
+    f = ""
+    if num < 5 then print("You have "..6-num.." incorrect attempts left") end
+    if num >= 1 then a = "😔" end
+    if num >= 2 then b = "|" end
+    if num >= 3 then c = "/" end
+    if num >= 4 then d = "\\" end
+    if num >= 5 then e = "/" end
+    if num == 5 then print("You have 1 attempt left") end
+    if num == 6 then f = "\\" end
+    print("    ___________")
+    print("    |         |")
+    print("    |         "..a)
+    print("    |        "..c..b..d)
+    print("    |        "..e.." "..f)
+    print("    |")
+    print("    |")
+    print("    |")
+    print("    |")
+    print("    |")
+    print("____|____")
 end
 
 function gallow_test()
@@ -74,19 +77,149 @@ function gallow_test()
   end
 end
 
-function test_words()
---test_words returns a table of 10 words, and the count (10)
-  words = {}
-  words[1] = "apple"
-  words[2] = "banana"
-  words[3] = "cactus"
-  words[4] = "dog"
-  words[5] = "empty"
-  words[6] = "food"
-  words[7] = "google"
-  words[8] = "hello"
-  words[9] = "indigo"
-  words[10] = "jumping"
-  count = table_size(words)
-  return words, count
+function print_wrong(letters_guessed, wrong_ct)
+--This function prints out the players incorrect letter guesses.
+--It is in order of appearance.
+    print("Guessed incorrect letters:")
+    for i = 0, wrong_ct-1 do
+        io.write(letters_guessed[i].." ")
+    end
+end
+
+function hangman (words)
+    --calculate size of table
+    words_length = table_size(words)
+
+    --This section finds a random index of the table, meaning the word is randomly picked.
+    math.randomseed(os.time()) --need to run first
+    random_index = math.random(1,words_length)
+    word = words[random_index]
+    word_length = string.len( word )
+
+    --This initializes each letter of the word to false as a way to test if the
+    --user inputs a correct letter later in the code.
+    show_letter = {} --better name for this variable??
+    for i = 1, word_length do --this initalized them to false
+        show_letter[i] = false
+    end
+
+    --Empty table that will store the users guesses.
+    letters_guessed = {}
+
+    wrong_ct = 0 --number of times user guessed wrong
+    correct_ct = 0--number of times user guessed correctly
+    word = words[random_index]
+
+    --I made the line printing a function that calls gallow with the attempt
+    --We could probably make a game function and just have the game in a loop
+
+    --This function is what runs the game.
+
+    while (wrong_ct < 6) and (correct_ct < string.len(word)) do
+        --this while loop runs until the man in hanged or the user gets the word.
+        gallow(wrong_ct)
+        io.write("Your word is:\n")
+        for i = 1, string.len(word) do --prints underscore for hidden letters and
+            if show_letter[i] then     --prints the letters if guessed correctly
+                io.write(string.sub(word,i,i).." ")--we would print the ith character
+            else
+                io.write("_ ")
+            end
+        end
+        print()
+        print_wrong(letters_guessed, wrong_ct)
+        print()
+
+      --This section of code takes users guesses and checks whether a wrong input
+      --has been guessed before. We decided that letters that have been shown
+      --should not display that output and we just continue to show the current game.
+        io.write("Please guess a letter: ")
+        ch = io.read("*line"); --reads the input
+        ch = string.upper(string.sub(ch,1,1) )
+        char_found = false;
+        char_unique = false;
+        while not char_unique do
+            char_unique = true
+            for k = 0, wrong_ct do -- This loop checks if already incorrect guess
+                if letters_guessed[k] == ch then --checks if letter has been guessed
+                    io.write("You already guessed that, input a new letter:")
+                    ch = io.read("*line");
+                    ch = string.upper( string.sub(ch,1,1) )
+                    char_unique = false
+                end
+            end
+            for i = 1, word_length do -- This loop checks if already correct guess
+                if string.upper(string.sub(word,i,i)) == ch and show_letter[i] then
+                    io.write("That letter is already in the word, input new letter: ")
+                    ch = io.read("*line");
+                    ch = string.upper( string.sub(ch,1,1) )
+                    char_unique = false
+                end
+            end
+            if ch == ' ' or ch == '\0' then
+                --This check for space, ??check for enter??
+                print("THIS IS A SPACE OR ENTER")
+                io.write("Invalid character, input new letter: ")
+                ch = io.read("*line");
+                ch = string.upper( string.sub(ch,1,1) )
+                char_unique = false
+            end
+        end
+     --This section of code checks whether the user input is correct. If it is
+     --correct, we make that index of the word true, then increment the number of
+     --correctly counted words.
+        for i = 1, string.len(word) do
+            --compares letter in word to the users guess
+            if string.upper(string.sub(word,i,i)) == ch then
+                if show_letter[i] then
+                    char_found = true
+                else
+                    char_found = true
+                    show_letter[i] = true;
+                    correct_ct = correct_ct + 1
+                end
+            end
+        end
+
+        if not char_found then
+            letters_guessed[wrong_ct] = ch
+            wrong_ct = wrong_ct + 1
+        end
+        print()
+    end
+
+    gallow(wrong_ct)
+    io.write("Your word is:\n")
+    for i = 1, string.len(word) do
+        if show_letter[i] or wrong_ct == 6 then
+            io.write(string.sub(word,i,i).." ")--here we would print the ith character
+        else
+            io.write("_ ")
+        end
+    end
+    print()
+    print_wrong(letters_guessed, wrong_ct)
+    print()
+    print()
+
+    if wrong_ct < 6 then
+        game_won = true
+        print("Congratulations, you guessed the word!")
+    else
+        game_won = false
+        print("Sorry, you lost! The word was: ", word)
+    end
+    print()
+
+    --END HANGMAN
+
+    io.write("Would you like to play another round? Yes or No: ")
+    ch = io.read("*line")
+    ch = string.upper( string.sub(ch,1,1) )
+    if ch == 'Y' then
+        play_again = true
+    else
+        play_again = false
+    end
+    return play_again, game_won
 end
